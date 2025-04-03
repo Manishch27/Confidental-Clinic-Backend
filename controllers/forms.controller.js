6820341cd5d0a30c850f106a9f48b28d44dbe0ba
Manish Chaudhary <manishchaudhary2705@gmail.com>
2025-04-03 13:00:28 +0530

    change the email to receive the form data

--- controllers/forms.controller.js
+++ controllers/forms.controller.js
@@ -22,7 +22,7 @@ export const popupForm = async(req,res)=>{
     const {fullName, phone, nearestClinic} = req.body;
     const mailOptions = {
         from: process.env.EMAIL_USER,
-        to: "manishchaudhary75059@gmail.com",
+        to: "myconfidentalclinic@gmail.com",
         subject: "New CTA form submission",
         text: `Full Name: ${fullName}\n\n Phone Number: ${phone}\n\n Nearest Clinic: ${nearestClinic}` 
     };
@@ -35,26 +35,26 @@ export const popupForm = async(req,res)=>{
     }
 };
 
-// Appointment Form endpoint
+// // Appointment Form endpoint
 
-export const appointmentForm = async(req, res)=>{
-    const {location, doctor, appointmentDate, appointmentTime, visited, fullName, phone, email} = req.body;
+// export const appointmentForm = async(req, res)=>{
+//     const {location, doctor, appointmentDate, appointmentTime, visited, fullName, phone, email} = req.body;
 
-    const mailOptions ={
-    from: process.env.EMAIL_USER,
-    to: "manishchaudhary75059@gmail.com",
-    subject: "Attention! Appointment Booked",
-    text: `Clinic Location: ${location}\n\n Doctor: ${doctor}\n\n Appointment Date: ${appointmentDate}\n\n Appointment Time: ${appointmentTime}\n\n Visited us before: ${visited}\n\n Full Name: ${fullName}\n\n Phone Number: ${phone} ${email!=undefined?("Email: ", email): ""}`
-    };
+//     const mailOptions ={
+//     from: process.env.EMAIL_USER,
+//     to: "myconfidentalclinic@gmail.com",
+//     subject: "Attention! Appointment Booked",
+//     text: `Clinic Location: ${location}\n\n Doctor: ${doctor}\n\n Appointment Date: ${appointmentDate}\n\n Appointment Time: ${appointmentTime}\n\n Visited us before: ${visited}\n\n Full Name: ${fullName}\n\n Phone Number: ${phone} ${email!=undefined?("Email: ", email): ""}`
+//     };
 
-    try {
-        transporter.sendMail(mailOptions);
-        res.status(200).json({message: "Your appointment booked successfully!"});
-    } catch (err) {
-        res.status(500).json({message: "error occured, please try again!", err});
-    }
+//     try {
+//         transporter.sendMail(mailOptions);
+//         res.status(200).json({message: "Your appointment booked successfully!"});
+//     } catch (err) {
+//         res.status(500).json({message: "error occured, please try again!", err});
+//     }
 
-}
+// }
 
 // Contact Us form
 
@@ -63,7 +63,7 @@ export const contactForm = async(req, res)=>{
     console.log(req.body);
     const mailOptions = {
         from: process.env.EMAIL_USER,
-        to: "manishchaudhary75059@gmail.com",
+        to: "myconfidentalclinic@gmail.com",
         subject: "New Form submission",
         text: `Full Name: ${fullName}\n\n Phone Number: ${phoneNumber}\n\n preferred Clinic: ${preferredClinic}\n\n subject: ${subject}\n\n message: ${message}\n\n ${email!=undefined? ("Email: ", email): ""}`
     };
