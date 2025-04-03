c04933bc978af2d1825ea212a9d226c24813d1a6
Manish Chaudhary <manishchaudhary2705@gmail.com>
2025-04-03 13:27:58 +0530

    change the email to receive the form data

--- routes/forms.router.js
+++ routes/forms.router.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {popupForm} from "../controllers/forms.controller.js";
-import { appointmentForm } from '../controllers/forms.controller.js';
+// import { appointmentForm } from '../controllers/forms.controller.js';
 import { contactForm } from '../controllers/forms.controller.js';
 
 const formsRouter = express.Router();
 
 formsRouter.post("/popup-form", popupForm);
-formsRouter.post("/appointment-form", appointmentForm);
+// formsRouter.post("/appointment-form", appointmentForm);
 formsRouter.post("/contact-form", contactForm);
 
 
